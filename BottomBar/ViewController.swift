87d107cfd537cac9bb3b62f440164ecce5068af8
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-02-26 16:43:56 +0300

    Second Commit

--- BottomBar/ViewController.swift
+++ BottomBar/ViewController.swift
@@ -33,13 +33,36 @@ class ViewController: UIViewController {
     @objc func setupNavigationBar() {
         let tabBarVC = UITabBarController()
         
-        let vc1 = FirstViewController()
-        let vc2 = SecondViewController()
-        let vc3 = ThirdViewController()
-        let vc4 = FourthViewController()
-        let vc5 = FifthViewController()
+        let vc1 = UINavigationController(rootViewController: FirstViewController())
+        let vc2 = UINavigationController(rootViewController: SecondViewController())
+        let vc3 = UINavigationController(rootViewController: ThirdViewController())
+        let vc4 = UINavigationController(rootViewController: FourthViewController())
+        let vc5 = UINavigationController(rootViewController: FifthViewController())
+        
+        vc1.title = "Home"
+        vc2.title = "Notification"
+        vc3.title = "About"
+        vc4.title = "Contact"
+        vc5.title = "Settings"
         
         tabBarVC.setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
+        
+        guard let items = tabBarVC.tabBar.items else {
+            return
+        }
+        
+        let images = ["house", "bell", "book", "book.closed", "gear"]
+        
+        for x in 0..<items.count {
+            items[x].image = UIImage(systemName: images[x])
+        }
+        
+        let badgeValues = ["1", "2", "3", "32", "13"]
+        
+        for y in 0..<items.count {
+            items[y].badgeValue = badgeValues[y]
+        }
+        
         tabBarVC.modalPresentationStyle = .fullScreen
         present(tabBarVC, animated: true)
     }
